ac6d18e3b5c0b0681d38ea9c5f646f27a1c31892
dfinke <finked@hotmail.com>
2021-02-21 10:29:44 -0500

    tweak Excel examples

--- Examples/CreateExcelWorkbook.ps1
+++ Examples/CreateExcelWorkbook.ps1
@@ -9,8 +9,8 @@ Import-PSAdvantageConfig D:\temp\scratch\config.ps1 # imports config with GitHub
 $command = @'
 $url = 'https://raw.githubusercontent.com/dfinke/PSKit/master/sampleCsv/aapl.csv'
 
-Export-Excel -InputObject (Invoke-RestMethod $url | ConvertFrom-Csv) -Path appl.xlsx
+$ts = (Get-Date).ToString("yyyyMMddHHmmss")
+Export-Excel -InputObject (Invoke-RestMethod $url | ConvertFrom-Csv) -Path "appl-$($ts).xlsx"
 '@
 
 Invoke-Advantage -owner $owner -reponame $reponame -template importexcel-powershell -command $command
-
