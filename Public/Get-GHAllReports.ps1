c930097e9f4cf984daf999a1edd2cfd57362342a
dfinke <finked@hotmail.com>
2022-03-20 12:02:31 -0400

    first attempt at get all metrics into Excel

---
+++ Public/Get-GHAllReports.ps1
@@ -0,0 +1,23 @@
+function Get-GHAllReports {
+    <#
+        .SYNOPSIS
+        Gets all metrics from a GitHub repo, Releases, Issues, and Pull Requests. Exports to Excel
+
+        .Example 
+        Get-GHAllReports dfinke/importexcel
+    #>
+    param(
+        [Parameter(ValueFromPipeline)]
+        [string[]]$slug,
+        [ValidateSet("open", "closed", "all")]
+        $state = 'all',
+        $XLFilename = "./GitHubStats.xlsx",        
+        [int]$NumberOfPages = 1
+    )
+
+    Process {
+        Get-GHIssueReport -slug $slug -state $state -XLFilename $XLFilename -NumberOfPages $NumberOfPages
+        Get-GHPullRequestReport -slug $slug -state $state -XLFilename $XLFilename -NumberOfPages $NumberOfPages
+        Get-GHRelease -slug $slug -XLFilename $XLFilename -NumberOfPages $NumberOfPages -AsExcelReport
+    }
+}
